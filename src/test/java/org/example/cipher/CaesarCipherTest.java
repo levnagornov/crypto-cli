179b4d6f1970223681ce4ca555a11674953d551f
Lev Nagornov <levnagornov@yahoo.com>
2024-04-19 19:31:37 +0200

    feat: add decryption tests

--- src/test/java/org/example/cipher/CaesarCipherTest.java
+++ src/test/java/org/example/cipher/CaesarCipherTest.java
@@ -21,62 +21,128 @@ class CaesarCipherTest {
     }
 
     @Test
-    void encodeWithKeyZero() {
+    void encryptWithKeyZero() {
         caesarCipher = new CaesarCipher(alphabet, 0);
         assertEquals("ABC", caesarCipher.encrypt("ABC"));
     }
 
     @Test
-    void encodeWithSmallPositiveKey() {
+    void decryptWithKeyZero() {
+        caesarCipher = new CaesarCipher(alphabet, 0);
+        assertEquals("ABC", caesarCipher.decrypt("ABC"));
+    }
+
+    @Test
+    void encryptWithSmallPositiveKey() {
         caesarCipher = new CaesarCipher(alphabet, 2);
         assertEquals("CDE", caesarCipher.encrypt("ABC"));
+        caesarCipher = new CaesarCipher(alphabet, 2);
+        assertEquals("ZAB", caesarCipher.encrypt("XYZ"));
     }
 
     @Test
-    void encodeWithSmallNegativeKey() {
+    void decryptWithSmallPositiveKey() {
+        caesarCipher = new CaesarCipher(alphabet, 2);
+        assertEquals("ABC", caesarCipher.decrypt("CDE"));
+        caesarCipher = new CaesarCipher(alphabet, 2);
+        assertEquals("YZA", caesarCipher.decrypt("ABC"));
+    }
+
+    @Test
+    void encryptWithSmallNegativeKey() {
         caesarCipher = new CaesarCipher(alphabet, -2);
         assertEquals("ABC", caesarCipher.encrypt("CDE"));
+        caesarCipher = new CaesarCipher(alphabet, -2);
+        assertEquals("YZA", caesarCipher.encrypt("ABC"));
     }
 
     @Test
-    void encodeWithBigPositiveKey() {
+    void decryptWithSmallNegativeKey() {
+        caesarCipher = new CaesarCipher(alphabet, -2);
+        assertEquals("CDE", caesarCipher.decrypt("ABC"));
+    }
+
+    @Test
+    void encryptWithBigPositiveKey() {
         caesarCipher = new CaesarCipher(alphabet, 26);
         assertEquals("ABC", caesarCipher.encrypt("ABC"));
     }
 
     @Test
-    void encodeWithBigNegativeKey() {
+    void decryptWithBigPositiveKey() {
+        caesarCipher = new CaesarCipher(alphabet, 26);
+        assertEquals("ABC", caesarCipher.decrypt("ABC"));
+    }
+
+    @Test
+    void encryptWithBigNegativeKey() {
         caesarCipher = new CaesarCipher(alphabet, -26);
         assertEquals("ABC", caesarCipher.encrypt("ABC"));
     }
 
     @Test
-    void encodeWithGiantPositiveKey() {
-        caesarCipher = new CaesarCipher(alphabet, 78);
-        assertEquals("ABC", caesarCipher.encrypt("ABC"));
+    void decryptWithBigNegativeKey() {
+        caesarCipher = new CaesarCipher(alphabet, -26);
+        assertEquals("ABC", caesarCipher.decrypt("ABC"));
     }
 
     @Test
-    void encodeWithGiantNegativeKey() {
-        caesarCipher = new CaesarCipher(alphabet, -78);
-        assertEquals("ABC", caesarCipher.encrypt("ABC"));
+    void encryptWithGiantPositiveKey() {
+        caesarCipher = new CaesarCipher(alphabet, 79);
+        assertEquals("BCD", caesarCipher.encrypt("ABC"));
     }
 
     @Test
-    void encodeIsCaseSensitive() {
+    void decryptWithGiantPositiveKey() {
+        caesarCipher = new CaesarCipher(alphabet, 79);
+        assertEquals("ABC", caesarCipher.decrypt("BCD"));
+    }
+
+    @Test
+    void encryptWithGiantNegativeKey() {
+        caesarCipher = new CaesarCipher(alphabet, -79);
+        assertEquals("ABC", caesarCipher.encrypt("BCD"));
+    }
+
+    @Test
+    void decryptWithGiantNegativeKey() {
+        caesarCipher = new CaesarCipher(alphabet, -79);
+        assertEquals("BCD", caesarCipher.decrypt("ABC"));
+    }
+
+    @Test
+    void encryptIsCaseSensitive() {
         caesarCipher = new CaesarCipher(alphabet, 1);
         assertEquals("Efg", caesarCipher.encrypt("Def"));
     }
 
     @Test
-    void encodeSkipsNonLetters() {
+    void decryptIsCaseSensitive() {
+        caesarCipher = new CaesarCipher(alphabet, 1);
+        assertEquals("Def", caesarCipher.decrypt("Efg"));
+    }
+
+    @Test
+    void encryptSkipsNonLetters() {
         caesarCipher = new CaesarCipher(alphabet, 1);
         assertEquals("?EFG .! OP1", caesarCipher.encrypt("?DEF .! NO1"));
     }
 
     @Test
-    void encodeThrowsExceptionWhenLetterIsNotInAlphabet() {
+    void decryptSkipsNonLetters() {
+        caesarCipher = new CaesarCipher(alphabet, 1);
+        assertEquals("?DEF .! NO1", caesarCipher.decrypt("?EFG .! OP1"));
+    }
+
+    @Test
+    void encryptThrowsExceptionWhenLetterIsNotInAlphabet() {
         caesarCipher = new CaesarCipher(alphabet, 1);
         assertThrows(RuntimeException.class, () -> caesarCipher.encrypt("XYZЭЮЯ"));
+    }
+
+    @Test
+    void decryptThrowsExceptionWhenLetterIsNotInAlphabet() {
+        caesarCipher = new CaesarCipher(alphabet, 1);
+        assertThrows(RuntimeException.class, () -> caesarCipher.decrypt("XYZЭЮЯ"));
     }
 }
